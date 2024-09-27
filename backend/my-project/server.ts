db00c0e77ec4d025fbb5e4750b7ac3c96a9bc84c
Max Blennemann <80426654+23maxb@users.noreply.github.com>
2024-09-27 12:36:35 -0700

    added .env support for deno

--- backend/my-project/server.ts
+++ backend/my-project/server.ts
@@ -1,9 +1,33 @@
 import express from "npm:express@4.21.0";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import axios from "npm:axios@1.7.7";
+import env = Deno.env;
+
+async function getCourses() {
+    
+    const CANVAS_LMS_KEY = env.get("CANVAS_LMS_KEY");
+    try {
+        console.log(CANVAS_LMS_KEY);
+        const response = await axios.get(
+            "https://canvas.instructure.com/api/v1/courses",
+            {
+                headers: {
+                    "Authorization": `Bearer ${CANVAS_LMS_KEY}`,
+                },
+            },
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching courses:", error);
+    }
+}
+
 export function server(): number {
     const app = express();
 
-    app.get("/", (req, res) => {
-        res.send("Welcome to the Dinosaur API!");
+    app.get("/", async (req: string, res: { send: (arg0: void) => void }) => {
+        let a = getCourses();
+        res.send(await a);
     });
 
     app.listen(8000);
